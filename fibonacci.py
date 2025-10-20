b22324e1e9c0de66e7d92a4e1f43091dcb0008b4
Rafay Hussain <rhussa02@uoguelph.ca>
2025-10-20 13:04:37 -0400

    fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -16,19 +16,22 @@ def get_number():
             return number  # returning the valid number 
         else:
             print("Please enter a number greater than 0.")  # message if the number is 0 or negative
-        else:
-            print("Please enter a valid number.")  # message if the input is not a number
+    else:
+        print("Please enter a valid number.")  # message if the input is not a number
 
-# defining the first 2 numbers in the Fibonacci sequence
-a = 0
-b = 1
+#function for printing the Fibonacci sequence
+def print_fibonacci(number):
+  # defining the first 2 numbers in the Fibonacci sequence
+  a = 0
+  b = 1
 
-# printing the Fibonacci sequence
-for i in range(number):  # loop runs for the number of terms the user entered
-    print(a, end=" ")  # printing the current number on one line with spaces
-    next_number = a + b  # calculating the next number
-    a = b  # moving forward in the sequence
-    b = next_number  # updating b to the next number
+  # printing the Fibonacci sequence
+  for i in range(number):  # loop runs for the number of terms the user entered
+      print(a, end=" ")  # printing the current number on one line with spaces
+      next_number = a + b  # calculating the next number
+      a = b  # moving forward in the sequence
+      b = next_number  # updating b to the next number
+  print()  # printing a newline at the end
 
 # creating the main function
 def main():
@@ -38,4 +41,5 @@ def main():
     #calling the print_fibonacci() function and passing the user's number to it
     print_fibonacci(number) # this function will generate and display the Fibonacci sequence
 
-main() #calling the main function to run the program 
+main() #calling the main function to run the program
+
